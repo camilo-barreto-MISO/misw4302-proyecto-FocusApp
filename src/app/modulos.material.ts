fa9c2116858d57cddab99822c8f81e38ac021ef1
Camilo Barreto <fc.barreto@uniandes.edu.co>
2024-09-25 18:38:27 -0500

    Se agrega la pantalla de comenzar tarea movil

--- src/app/modulos.material.ts
+++ src/app/modulos.material.ts
@@ -4,6 +4,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -18,4 +19,5 @@ export const ModulosMaterial = [
   MatListModule,
   MatToolbarModule,
   MatSlideToggleModule,
+  MatProgressSpinnerModule,
 ];
