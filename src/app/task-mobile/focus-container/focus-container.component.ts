2d38d760b8b216bd41f86d4aa8eaa369de14a1ba
Augusto Romero Arango <a.romeroa23@uniandes.edu.co>
2024-09-25 19:03:08 -0500

    Se agrega la cuenta regresiva

--- src/app/task-mobile/focus-container/focus-container.component.ts
+++ src/app/task-mobile/focus-container/focus-container.component.ts
@@ -1,17 +1,137 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { ModulosMaterial } from '../../modulos.material';
 
+enum TaskStatus {
+  startTask = 'start',
+  running = 'running',
+  startBreak = 'startBreak',
+}
+
 @Component({
   selector: 'app-focus-container',
   standalone: true,
-  imports: [ModulosMaterial, ReactiveFormsModule],
+  imports: [ModulosMaterial, ReactiveFormsModule, CommonModule],
   templateUrl: './focus-container.component.html',
-  styleUrl: './focus-container.component.scss',
+  styleUrls: ['./focus-container.component.scss'],
 })
 export class FocusContainerComponent {
+  TaskStatus = TaskStatus;
+  statusFocus: TaskStatus = TaskStatus.startTask;
+  intervalId!: number;
+  isPaused: boolean = false;
+
   form: FormGroup = new FormGroup({
-    hour: new FormControl('25'),
-    minute: new FormControl('00'),
+    minutes: new FormControl('25'),
+    seconds: new FormControl('00'),
   });
+
+  get seconds() {
+    return parseInt(this.form.get('seconds')?.value, 10);
+  }
+
+  get minutes() {
+    return parseInt(this.form.get('minutes')?.value, 10);
+  }
+
+  get statusText(): string {
+    switch (this.statusFocus) {
+      case TaskStatus.startTask:
+        return '1 de 4 sesiones';
+      case TaskStatus.running:
+        return 'Focus...';
+      case TaskStatus.startBreak:
+        return '¡Es tiempo del descanso 1 de 4!';
+      default:
+        return 'Estado desconocido';
+    }
+  }
+
+  get mostrarCargando() {
+    return (
+      (this.isRunning && !this.isPaused) ||
+      (this.isStartBreak && !this.isPaused)
+    );
+  }
+
+  get isRunning(): boolean {
+    return this.statusFocus === TaskStatus.running;
+  }
+
+  get isStartBreak(): boolean {
+    return this.statusFocus === TaskStatus.startBreak;
+  }
+
+  constructor() {
+    this.validateStatus();
+  }
+
+  validateStatus() {
+    if (this.isStartBreak) {
+      this.form.patchValue({
+        minutes: '05',
+        seconds: '00',
+      });
+    }
+  }
+
+  startTask() {
+    this.statusFocus = TaskStatus.running;
+    this.startTimer();
+  }
+
+  startTimer() {
+    this.intervalId = window.setInterval(() => {
+      this.updateTimer();
+    }, 1000);
+  }
+
+  updateTimer() {
+    let minutes = this.minutes;
+    let seconds = this.seconds;
+
+    if (seconds === 0) {
+      if (minutes === 0) {
+        this.stopTimer();
+        this.statusFocus = TaskStatus.startBreak;
+      } else {
+        minutes--;
+        seconds = 59;
+      }
+    } else {
+      seconds--;
+    }
+
+    this.updateForm(minutes, seconds);
+  }
+
+  updateForm(minutes: number, seconds: number) {
+    this.form.patchValue({
+      minutes: minutes.toString().padStart(2, '0'),
+      seconds: seconds.toString().padStart(2, '0'),
+    });
+  }
+
+  toggleTimer() {
+    if (this.intervalId) {
+      this.stopTimer();
+      this.isPaused = true;
+    } else {
+      this.isPaused = false;
+      this.startTimer();
+    }
+  }
+
+  stopTimer() {
+    clearInterval(this.intervalId);
+    this.intervalId = 0;
+  }
+
+  startBreak() {
+    this.isPaused = true;
+    this.statusFocus = TaskStatus.startBreak;
+    this.validateStatus();
+    this.stopTimer();
+  }
 }
